a5d701821d02c906a5287f737a8e145bddcab90c
JosephMillsAtWork <josephjamesmills2@gmail.com>
2016-07-18 16:19:24 -0700

    add leveldb to the array

--- osxBuildHelper-stage1.sh
+++ osxBuildHelper-stage1.sh
@@ -33,7 +33,7 @@ QTDIR=$2
 EXQMLLIBSARRAY=($QTDIR/qml/ProRotoQml $QTDIR/qml/Communi $QTDIR/qml/Material $QTDIR/qml/QtQuick )
 
 ## Add more if needed
-EXLIBSARRAY=($QTDIR/lib/IrcCore.framework $QTDIR/lib/IrcModel.framework $QTDIR/lib/IrcUtil.framework $QTDIR/lib/libprotoblock-core.1.0.0.dylib )
+EXLIBSARRAY=($QTDIR/lib/IrcCore.framework $QTDIR/lib/IrcModel.framework $QTDIR/lib/IrcUtil.framework $QTDIR/lib/libprotoblock-core.1.0.0.dylib /usr/local/opt/leveldb/lib/libleveldb.1.dylib)
 
 
 ###########
